f494c5da09b36fe16fde2fb27b0dbe12eed46c43
starqazstar <101437289+starqazstar@users.noreply.github.com>
2023-09-09 15:28:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -38,25 +38,23 @@ const nextConfig = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', {
-                modules: 'commonjs', // 转换 ES6 模块到 CommonJS
-                targets: '> 0.25%, not dead', // 根据需要选择目标浏览器
-              }],
-              '@babel/preset-react', // 如果你使用 React，可以添加此预设
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties', // 为类属性添加支持
-              '@babel/plugin-proposal-private-methods', // 为私有方法添加支持
-              // 你可以根据需要添加更多的 Babel 插件
-            ],
+          ['@babel/preset-env', {
+            "modules": "commonjs",
+            "targets": {
+              "esmodules": true
+            }
+          }]
+        ],
+        plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-syntax-dynamic-import']
+    
           },
         },
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
       }
     );
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
     return config;
   },
